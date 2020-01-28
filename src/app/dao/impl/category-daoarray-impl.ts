105d89f4480c8acb3f756c8913f40471f2630a3c
group-bbv181 <krushyura@gmail.com>
2020-01-28 21:19:07 +0200

    DAObrench

--- src/app/dao/impl/category-daoarray-impl.ts
+++ src/app/dao/impl/category-daoarray-impl.ts
@@ -1,2 +1,34 @@
-export class CategoryDAOArrayImpl {
+import {CategoryDAO} from '../interface/category-dao';
+import {Observable, of} from 'rxjs';
+import {Category} from '../../model/category';
+import {TestData} from '../../data/TestData';
+
+export class CategoryDAOArrayImpl implements CategoryDAO{
+  add(T): Observable<Category> {
+    return undefined;
+  }
+
+  delete(id: number): Observable<Category> {
+    return undefined;
+  }
+
+  get(id: number): Observable<Category> {
+    return undefined;
+  }
+
+  getAll(): Observable<Category[]> {
+
+    return of(TestData.categories);
+  }
+
+  search(title: string): Observable<Category> {
+    return undefined;
+  }
+
+  update(T): Observable<Category> {
+    return undefined;
+  }
+
 }
+
+
--- src/app/view/categories/categories.component.ts
+++ src/app/view/categories/categories.component.ts
@@ -15,17 +15,18 @@ export class CategoriesComponent implements OnInit {
   constructor(private dataHandlerService: DataHandlerService) { }
 
   ngOnInit() {
-    //this.categories = this.dataHandlerService.getCategories();
-    //console.log(this.categories);
+    this.dataHandlerService.getAllTasks().subscribe(categories => this.categories = categories);
+    console.log(this.categories);
+
   }
 
- /* getTasksByCategory(category: Category) {
+  getTasksByCategory(category: Category) {
     this.dataHandlerService.getTasks().filter(task => task.category === category);
   }
 
   showTasksByCategory(category: Category) {
     this.selected = category;
     this.dataHandlerService.fetchTasksByCategory(category);
-  }*/
+  }
 
 }
